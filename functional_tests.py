294f528dea4f286e9ed445ffc41b2577832531e1
StevenSLXie <stevenslxie@gmail.com>
2014-01-04 10:21:05 +0800

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,26 +1,54 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase): #1
+class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self): #2
+    def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-    def tearDown(self): #3
-		self.browser.quit()
-		#pass
+    def tearDown(self):
+        self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self): #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title) #5
-        self.fail('Finish the test!') #6
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-		#[...rest of comments as before]
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
-if __name__ == '__main__': #7
-    unittest.main() #8
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list
+		#[...]
+
+if __name__ == '__main__':
+	unittest.main()
