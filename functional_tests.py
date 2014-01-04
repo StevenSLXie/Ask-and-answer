7031da83a5308431db5f375118d701f82ec4f41a
StevenSLXie <stevenslxie@gmail.com>
2014-01-04 11:07:42 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -9,7 +9,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        self.browser.quit()
+		self.browser.quit()
+		#pass
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -39,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
