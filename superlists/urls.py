28479881162eb7a611ab69b071347495c7edf2ad
StevenSLXie <stevenslxie@gmail.com>
2014-01-05 15:58:37 +0800

    tidy up the URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-	url(r'^lists/(\d+)/$','lists.views.view_list',name='view_list'),
-    url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
-	url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+	url(r'^lists/',include('lists.urls')),
+	#url(r'^lists/(\d+)/$','lists.views.view_list',name='view_list'),
+	#url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
+	#url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     # url(r'^blog/', include('blog.urls')),
 
 	#url(r'^admin/', include(admin.site.urls)),
